374eebc0d95bab6d5029aa78cf2594a12967f524
blazion09 <muhd09afiq@gmail.com>
2024-05-12 15:10:35 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-// import "./css/reset.css";
+import "./css/reset.css";
 import "./css/style.css";
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => e.preventDefault);
