f7cadd52e9c43caf92b132ceee8b836f842a04cb
blazion09 <muhd09afiq@gmail.com>
2024-05-13 09:54:41 +0800

    feat: add country selection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./css/reset.css";
 import "./css/style.css";
+import "./js/crs.min";
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => e.preventDefault);
